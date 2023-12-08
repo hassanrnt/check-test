7e58fdd8d43f69c8685f88430e643ac2532aa38f
hassanrnt <hassanritouny97@gmail.com>
2023-12-08 22:34:54 +0100

    first commit

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,24 +1,18 @@
+import React, { useState } from 'react';
 function Card(props){
-   const ustate=()=> {
-      console.log("hello world");
-   }
+  const [showImage, setShowImage] = useState(false);
 
-    if(!props.person.shows){
-      return(
-        <p style={{fontSize:"20px"}}>Not Found!!</p>
-      );
-    }
-    // ?
+  const CardImage = () => {
+    setShowImage(!showImage);
+  };
+
     return(
         <div
       className="card rounded-xl w-72 bg-white shadow-xl flex justify-start items-center gap-2 flex-col overflow-hidden"
     >
       <div className="w-full h-40 bg-sky-600 flex p-4 justify-center items-center">
-        <img
-          src={props.person.imgSrc}
-          alt=""
-          className="aspect-square rounded-full h-full object-cover"
-        />
+        {showImage && <img src={props.person.imgSrc} alt={props.person.fullName} className="aspect-square rounded-full h-full object-cover"/>}
+        
       </div>
       <div
         className="text-content p-2 w-full flex text-center flex-col gap-2 justify-center items-center"
@@ -31,7 +25,9 @@ function Card(props){
           className="bg-sky-600 hover:bg-sky-700 text-white px-4 py-2 rounded-md shadow-md"
         >
           {/* ADD  onClick={ustate}*/}
-          <a href="#" onClick={ustate}>View Profile</a>
+          <a href="#" onClick={CardImage}>
+            {showImage ? 'Display l\'image' : 'Hide l\'image'}
+          </a>
         </button>
       </div>
     </div>
--- src/personne.js
+++ src/personne.js
@@ -1,39 +1,39 @@
 const person = [
-    {
-       "fullName": "John Doe",
-        "bio": "A software engineer with a passion for technology.",
-        "imgSrc": 'https://randomuser.me/api/portraits/men/88.jpg',
-        "profession": "Software Engineer",
-        "shows": true
-      },
-    {
-       "fullName": "John Doe",
-        "bio": "A software engineer with a passion for technology.",
-        "imgSrc":'https://randomuser.me/api/portraits/women/64.jpg',
-        "profession": "Software Engineer",
-        "shows": true
-      },
-    {
-       "fullName": "John Doe",
-        "bio": "A software engineer with a passion for technology.",
-        "imgSrc": 'https://randomuser.me/api/portraits/women/90.jpg',
-        "profession": "Software Engineer",
-        "shows": false
-      },
-    {
-       "fullName": "John Doe",
-        "bio": "A software engineer with a passion for technology.",
-        "imgSrc": 'https://randomuser.me/api/portraits/women/54.jpg',
-        "profession": "Software Engineer",
-        "shows": true
-      },
-    {
-       "fullName": "John Doe",
-        "bio": "A software engineer with a passion for technology.",
-        "imgSrc": 'https://randomuser.me/api/portraits/men/75.jpg',
-        "profession": "Software Engineer",
-        "shows": false
-      },
+  {
+     "fullName": "John Doe",
+      "bio": "A software engineer with a passion for technology.",
+      "imgSrc": 'https://randomuser.me/api/portraits/men/88.jpg',
+      "profession": "Software Engineer",
+      "shows": true
+    },
+  {
+     "fullName": "Alexia Doe",
+      "bio": "A software engineer with a passion for technology.",
+      "imgSrc":'https://randomuser.me/api/portraits/women/64.jpg',
+      "profession": "Software Engineer",
+      "shows": true
+    },
+  {
+     "fullName": "Amanda Doe",
+      "bio": "A software engineer with a passion for technology.",
+      "imgSrc": 'https://randomuser.me/api/portraits/women/90.jpg',
+      "profession": "Software Engineer",
+      "shows": false
+    },
+  {
+     "fullName": "Aicha Doe",
+      "bio": "A software engineer with a passion for technology.",
+      "imgSrc": 'https://randomuser.me/api/portraits/women/54.jpg',
+      "profession": "Software Engineer",
+      "shows": true
+    },
+  {
+     "fullName": " Clarence Doe",
+      "bio": "A software engineer with a passion for technology.",
+      "imgSrc": 'https://randomuser.me/api/portraits/men/75.jpg',
+      "profession": "Software Engineer",
+      "shows": false
+    },
 
 ]
 export default  person;
